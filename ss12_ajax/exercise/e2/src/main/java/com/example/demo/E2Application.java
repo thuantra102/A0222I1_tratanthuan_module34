728929ddf4bce1f6ae7a16d16a67755d43efdc84
thuantra102 <thuantra102@gmail.com>
2022-11-09 15:00:27 +0700

    update

---
+++ ss12_ajax/exercise/e2/src/main/java/com/example/demo/E2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E2Application.class, args);
+    }
+
+}
---
+++ ss12_ajax/exercise/e2/src/main/java/com/example/demo/service/impl/SmartphoneService.java
@@ -0,0 +1,35 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Smartphone;
+import com.example.demo.repository.ISmartphoneRepository;
+import com.example.demo.service.ISmartphoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SmartphoneService implements ISmartphoneService {
+    @Autowired
+    private ISmartphoneRepository smartPhoneRepository;
+
+    @Override
+    public Iterable<Smartphone> findAll() {
+        return smartPhoneRepository.findAll();
+    }
+
+    @Override
+    public Optional<Smartphone> findById(Long id) {
+        return smartPhoneRepository.findById(id);
+    }
+
+    @Override
+    public Smartphone save(Smartphone smartPhone) {
+        return smartPhoneRepository.save(smartPhone);
+    }
+
+    @Override
+    public void remove(Long id) {
+        smartPhoneRepository.deleteById(id);
+    }
+}
